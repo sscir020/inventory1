291d3bbb9b499c90d0e1a7111eb3a12a71802f8f
sscir020 <sscir020@live.cn>
2018-11-01 17:26:20 +0800

    update add cancel opr

--- app/ctr/errors.py
+++ app/ctr/errors.py
@@ -6,10 +6,10 @@ from ..__init__ import db#session
 def page_not_found(e):
     db.session.close()
     db.session.rollback()
-    return "not found 404" + e
+    return e
 
 @ctr.app_errorhandler(500)
 def internal_error(e):
     db.session.close()
     db.session.rollback()
-    return "internal 500" + e
+    return e
